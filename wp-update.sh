e25ced6e76a7cc88cc15642c8b0635dd59cf8934
lunaluna <web@lunalunadesign.com>
2016-05-21 06:56:37 +0900

    add check installation

--- wp-update.sh
+++ wp-update.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+
+if type wp >/dev/null 2>&1; then
+  echo >&2 "wp-cli がインストールされていません";
+  exit 1;
+fi
+
+if type git >/dev/null 2>&1; then
+  echo >&2 "git がインストールされていません";
+  exit 1;
+fi
+
 wp plugin update --all
 wp theme update --all
 wp core update
